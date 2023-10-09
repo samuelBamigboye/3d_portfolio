a849787087cb71e9a6aabd36527dc02b36d4962e
Samuel Bamigboye <samdlery65@gmail.com>
2023-10-09 19:04:45 +0100

    Create assest, component, public, and utils

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    {/* <link rel="shortcut icon" href="https://photos.google.com/photo/AF1QipNp7rhOOINySPuRRvxVaELav6V9fr9Xjk1Vg4oS" type="image/x-icon" /> */}
   </React.StrictMode>,
 )
