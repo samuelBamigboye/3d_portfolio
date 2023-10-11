14adfe0d27766e5c3ca287ab7b01685937f9a3bb
Samuel Bamigboye <samdlery65@gmail.com>
2023-10-11 20:18:22 +0100

    modification

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,10 +26,11 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
+      
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center mb-[-90px]'>
+      <div className='absolute xs:bottom-10 bottom-42 w-full flex justify-center items-center mt-[20px]'>
         <a href='#about'>
-          <div id="about" className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 '>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 '>
             <motion.div
               animate={{
                 y: [0, 24, 0],
--- src/constants/index.js
+++ src/constants/index.js
@@ -167,7 +167,7 @@ const experiences = [
     iconBg: "#E6DEDD",
     date: "Jan 2023 - Present",
     points: [
-      "Developing and maintaining web applications using Next.js and other related technologies.",
+      "Developing and maintaining web applications using MERN Stack and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
